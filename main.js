f181e4e44b90f113f1372b07ed6fa14fc9ea5218
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-09-01 00:01:10 +0300

    finished section cards

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
 const gridBtn = document.getElementById("gridView");
 const listBtn = document.getElementById("listView");
 const gallery = document.querySelector(".gallery");
+const sortedSection = document.querySelector(".sorted-section");
+const pagination = document.querySelector(".pagination");
+const page = document.body;
 
-gridBtn.addEventListener("click", () => {
-  gallery.classList.remove("list-view");
-  gallery.classList.add("grid-view");
-  gridBtn.classList.add("active");
-  listBtn.classList.remove("active");
-});
+// функція перемикання вигляду
+function switchView(mode) {
+  if (mode === "grid") {
+    gallery.classList.remove("list-view");
+    gallery.classList.add("grid-view");
 
-listBtn.addEventListener("click", () => {
-  gallery.classList.remove("grid-view");
-  gallery.classList.add("list-view");
-  listBtn.classList.add("active");
-  gridBtn.classList.remove("active");
-});
+    sortedSection.classList.remove("list-style");
+    sortedSection.classList.add("grid-style");
+
+    pagination.classList.remove("list-style");
+
+    page.classList.remove("list-mode");
+    page.classList.add("grid-mode");
+
+    gridBtn.classList.add("active");
+    listBtn.classList.remove("active");
+  } else {
+    gallery.classList.remove("grid-view");
+    gallery.classList.add("list-view");
+
+    sortedSection.classList.remove("grid-style");
+    sortedSection.classList.add("list-style");
+
+    pagination.classList.add("list-style");
+
+    page.classList.remove("grid-mode");
+    page.classList.add("list-mode");
+
+    listBtn.classList.add("active");
+    gridBtn.classList.remove("active");
+  }
+}
+
+gridBtn.addEventListener("click", () => switchView("grid"));
+listBtn.addEventListener("click", () => switchView("list"));
